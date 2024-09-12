a6fc4ac9826d004eefce99b4a06c45a8af174e06
zetsuniiiii <ggwp5673113@gmail.com>
2024-09-12 15:26:48 +0800

    Subtraction

--- calculator.java
+++ calculator.java
@@ -10,4 +10,8 @@ public class calculator{
     public static int subtract(int a - int b){
         return a-b;
     }
+
+     public static int subtract(int a, int b) {
+        return a - b;
+    }
 }
