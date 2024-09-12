fe40677cd2e732f5fdea0eb4a7d36d7d79bc30bb
zetsuniiiii <ggwp5673113@gmail.com>
2024-09-12 15:39:55 +0800

    Multiplication

--- calculator.java
+++ calculator.java
@@ -10,13 +10,7 @@ public class calculator{
     public static int subtract(int a - int b){
         return a-b;
     }
-}
-public class calculator{
-    public static int multiply(int a, int b){
-        return a*b;
-    }
-}
-public static int multiply(int a, int b) {
+
+   public static int multiply(int a, int b) {
         return a * b;
     }
-    
