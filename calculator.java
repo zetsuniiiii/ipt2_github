9c0fff17a7d10960bc4e28304d0c65807dcd5bdc
zetsuniiiii <ggwp5673113@gmail.com>
2024-09-12 15:47:15 +0800

    MINUS

--- calculator.java
+++ calculator.java
@@ -5,7 +5,9 @@ public static int add(int a, int b) {
         return a + b;
     }
 
-    public static int subtract(int a, int b) {
+       public static int subtract(int a, int b) {
         return a - b;
     }
+
+    
 }
