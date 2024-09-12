97df0c061b351059c1f4308ed2ff30f187b3e5a0
zetsuniiiii <ggwp5673113@gmail.com>
2024-09-12 15:48:13 +0800

    TIMES

--- calculator.java
+++ calculator.java
@@ -11,6 +11,7 @@ public class calculator{
         return a-b;
     }
 
-   public static int multiply(int a, int b) {
+    public static int multiply(int a, int b) {
         return a * b;
     }
+}
